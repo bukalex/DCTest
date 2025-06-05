ea481032ec2e69871cdc5a3850e41a2effafd40f
Aleksei Gurylev <gurylevalexey@gmail.com>
2025-06-05 12:40:28 +0300

    Initial commit

---
+++ Source/DCTest/DCTest.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DCTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DCTest, "DCTest" );
